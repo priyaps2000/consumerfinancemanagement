0387e41300e856a71493be68bc74f79254005b83
Gunavardhan Reddy Akiti <86407997+gunavardhan360@users.noreply.github.com>
2022-11-06 12:57:23 +0530

    Update ProductRepository.java

--- src/main/java/com/wellsfargo/consumerfinancemanagement/repository/ProductRepository.java
+++ src/main/java/com/wellsfargo/consumerfinancemanagement/repository/ProductRepository.java
@@ -9,19 +9,7 @@ import com.wellsfargo.consumerfinancemanagement.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-	@Modifying
-	@Query(value = "UPDATE user SET actvn_status = ?2 WHERE user_name = ?1", nativeQuery = true)
-	void changeActStatus(String userName, String response);
-	
-	@Query(value = "SELECT password FROM admin a WHERE a.adminu_name = :userName", nativeQuery = true)
-	public String findPasswordByaName(@Param("userName") String userName);
-
 	@Query(value = "SELECT * FROM product p WHERE p.purchase_count = (SELECT MAX(p.purchase_count) FROM product p) LIMIT 1", nativeQuery = true)
 	public Product getOne();
-	
-//	@Modifying(clearAutomatically = true)
-//	@Query(value = "UPDATE user SET password = ?2 WHERE user_name = ?1", nativeQuery = true)
-//	void updatePwdByuName(String userName, String password);
-
 }
 
